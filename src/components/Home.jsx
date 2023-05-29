26c2b4fae12970cf90149ef79b64d0b92f37d75e
Булахов Владислав <vladislav.bulahov@yandex.ru>
2023-05-29 08:44:36 +0300

    moved Book button for center home page for mobile version

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Home.css"
 import myWallpaper from '../assets/img/wallpaper.png';
+import BookLink from "./partial/BookLink";
 
 const Home = () => {
+    const message = 'Hello. Could you help me with scooter?'
+
     return (
             <div className="home_container"
                  style={{
@@ -16,6 +19,10 @@ const Home = () => {
                     <p>
                         Discover Bali in style on our sleek and reliable scooters! Our wide selection of scooters offers you the freedom and adventure to explore the island your way. With easy booking, affordable rates, and dependable quality, renting a scooter has never been easier. So, book now and ride with style!
                     </p>
+                    <div className='show'>
+                        <BookLink message={message}/>
+                    </div>
+
                 </div>
             </div>
     );
